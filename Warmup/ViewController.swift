390baac412f7d37a51aabe06907fa0580cd4c78c
george ehrman <georgeehrman@georges-macbook-air.local>
2016-06-23 19:51:22 -0700

    Initial Commit

---
+++ Warmup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warmup
+//
+//  Created by george ehrman on 6/23/16.
+//  Copyright © 2016 george ehrman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
